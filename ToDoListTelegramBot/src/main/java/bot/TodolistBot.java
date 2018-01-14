a8dba553ea35ddba58188fde4533fa79b686860c
Andrey Lisovskiy <andrlis0506@gmail.com>
2018-01-15 01:29:08 +0300

    Fix bugs.

--- ToDoListTelegramBot/src/main/java/bot/TodolistBot.java
+++ ToDoListTelegramBot/src/main/java/bot/TodolistBot.java
@@ -38,45 +38,6 @@ public class TodolistBot extends TelegramLongPollingBot {
         if (update.hasMessage() && update.getMessage().hasText()) {
 
             analizeMessage(update);
-
-//            if (message.equals("/start") || message.equals("/show")) //В начале работы высвечиваются все списки (в функции надо сделать проверку)
-//                showTaskLists(update.getMessage().getChatId());
-//
-//            if (message.equals("/add")){                             //надо отделить команду от названия
-//                String nameList = "";
-//                String[] wordArray = message.split(" ");          //преобразую сообщения в массив строк
-//
-//                for(int i = 1; i <= wordArray.length; i++){
-//                    if (wordArray[i].equals(" "))                    // на тот случай если будет два пробела между словами
-//                        continue;
-//
-//                    if(i != wordArray.length)
-//                        nameList += wordArray[i] + " ";              // конкатенация собственно... если последнее то без пробела
-//                    else
-//                        nameList += wordArray[i];
-//                }
-//
-//                if(nameList.isEmpty()){
-//                    SendMessage errorMessage = new SendMessage()        //проверка на то, что хоть что-то ввели
-//                            .setChatId(update.getMessage().getChatId())
-//                            .setText("Введите имя списка");
-//                }
-//
-//
-//                addNewTaskList(update.getMessage().getChatId(),
-//                               nameList);
-//            }
-//
-//
-//
-//            SendMessage response = new SendMessage() // Create a SendMessage object with mandatory fields
-//                    .setChatId(update.getMessage().getChatId())
-//                    .setText(update.getMessage().getText());
-//            try {
-//                execute(response); // Call method to send the message
-//            } catch (TelegramApiException e) {
-//                e.printStackTrace();
-//            }
         }
     }
 
@@ -95,36 +56,41 @@ public class TodolistBot extends TelegramLongPollingBot {
             }
         }
 
-        SendMessage response = new SendMessage()
-                .setChatId(chat_id)
-                .setText(responseMsg);
+        sendMessage(responseMsg, chat_id);
+    }
 
+    private void deleteTaskList(int list_id) {  //если я правильно понимаю то Index в листе и id в таблице одни и те же
         try {
-            execute(response);
-        } catch (TelegramApiException e){
-            e.printStackTrace();
+            DAOFactory.getTaskListDao().removeTaskList(list_id);
+        }catch (DAOException exc){
+            exc.printStackTrace();
         }
     }
 
-    private void deleteTaskList(int list_id, Long chat_id) throws DAOException {  //если я правильно понимаю то Index в листе и id в таблице одни и те же
-         DAOFactory.getTaskListDao().removeTaskList(list_id);
+    private void changeTaskListName(int list_id, String newName, Integer user_id/*Long chat_id*/) {
+        try {
+            TaskList newList = DAOFactory.getTaskListDao().getTaskListByID(list_id);
+
+            newList.setListName(newName);
+            /*newList.setUserId(user_id);*/             //Зачем ставить user_id, если он уже есть
+
+            DAOFactory.getTaskListDao().updateTaskList(list_id, newList);
+        }catch (DAOException exc){
+            exc.printStackTrace();
+        }
     }
 
-    private void changeTaskListName(int list_id, String newName, Integer user_id/*Long chat_id*/) throws DAOException {
-        TaskList newList = DAOFactory.getTaskListDao().getTaskListByID(list_id);
-
-        newList.setListName(newName);
-        newList.setUserId(user_id);
-
-        DAOFactory.getTaskListDao().updateTaskList(list_id, newList);
-    }
-
-    private void addNewTaskList(Integer user_id, String listName, Long chat_id) throws DAOException {
+    private void addNewTaskList(Integer user_id, String listName) {
         TaskList newList = new TaskList();
 
         newList.setUserId(user_id);
         newList.setListName(listName);
 
+        try {
+            DAOFactory.getTaskListDao().saveTaskList(newList);
+        }catch (DAOException exc){
+            exc.printStackTrace();
+        }
     }
 
     private void showTasks(int list_id, Long chat_id){
@@ -142,55 +108,99 @@ public class TodolistBot extends TelegramLongPollingBot {
             }
         }
 
-        SendMessage response = new SendMessage()
-                    .setChatId(chat_id)
-                    .setText(responseMsg);
+        sendMessage(responseMsg, chat_id);
+    }
+
+    private void changeTaskStatus(int task_id, boolean status) {
         try {
-            execute(response);
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
+            Task changedTask = DAOFactory.getTaskDao().getTaskByID(task_id);
+            changedTask.setTaskStatus(status);
+
+            DAOFactory.getTaskDao().updateTask(task_id, changedTask);
+        }catch (DAOException exc){
+            exc.printStackTrace();
         }
     }
 
-    private void changeTaskStatus(int task_id, Long chat_id, boolean status) throws DAOException {
-        Task changedTask = DAOFactory.getTaskDao().getTaskByID(task_id);
-        changedTask.setTaskStatus(status);
-
-        DAOFactory.getTaskDao().updateTask(task_id, changedTask);
+    private void deleteTask(int task_id) {
+        try{
+            DAOFactory.getTaskDao().removeTask(task_id);
+        }catch (DAOException exc){
+            exc.printStackTrace();
+        }
     }
 
-    private void deleteTask(int task_id, Long chat_id) throws DAOException {
-        DAOFactory.getTaskDao().removeTask(task_id);
+    private void changeTaskText(int task_id, String newTaskText){
+        try{
+            Task changedTask = DAOFactory.getTaskDao().getTaskByID(task_id);
+            changedTask.setTaskDescription(newTaskText);
+
+            DAOFactory.getTaskDao().updateTask(task_id, changedTask);
+        }catch (DAOException exc){
+            exc.printStackTrace();
+        }
     }
 
-    private void changeTaskText(int task_id, String newTaskText, Long chat_id) throws DAOException {
-        Task changedTask = DAOFactory.getTaskDao().getTaskByID(task_id);
-        changedTask.setTaskDescription(newTaskText);
-
-        DAOFactory.getTaskDao().updateTask(task_id, changedTask);
-    }
-
-    private void addNewTask(int list_id, String taskDescription, Long chat_id) throws DAOException {
+    private void addNewTask(int list_id, String taskDescription) {
         Task newTask = new Task();
 
         newTask.setTaskDescription(taskDescription);
         newTask.setTaskStatus(false);
         newTask.setListId(list_id);
 
-        DAOFactory.getTaskDao().saveTask(newTask);
+        try {
+            DAOFactory.getTaskDao().saveTask(newTask);
+        }catch (DAOException exc){
+            exc.printStackTrace();
+        }
     }
 
     private void analizeMessage(Update update){
         String message = update.getMessage().getText();
 
+        checkUserStatement(update);
+
         if (message.toLowerCase().contains("/start") || message.toLowerCase().contains("/show")) { //В начале работы высвечиваются все списки (в функции надо сделать проверку)
             showTaskLists(update.getMessage().getFrom().getId(), update.getMessage().getChatId());
         }
         if (message.toLowerCase().contains("/addlist")) {
-            //addNewTaskList(update.getMessage().getFrom().getId(), );
+            UserStatement statement = getUserStatement(update.getMessage().getFrom().getId());
+            statement.setLastCommand("/addlist");
+            sendMessage("Enter new list`s title", update.getMessage().getChatId());
         }
         if (message.toLowerCase().contains("/deletelist")) {
-            //deleteTaskList();
+            UserStatement statement = getUserStatement(update.getMessage().getFrom().getId());
+            statement.setLastCommand("/deletelist");
+            sendMessage("Select list which you want to delete.", update.getMessage().getChatId());
+        }
+        if (message.toLowerCase().contains("/renamelist")){
+            UserStatement statement = getUserStatement(update.getMessage().getFrom().getId());
+            statement.setLastCommand("/renamelist");
+            sendMessage("Enter new title.", update.getMessage().getChatId());
+        }
+        if (message.toLowerCase().contains("/showtasks")){
+            UserStatement statement = getUserStatement(update.getMessage().getFrom().getId());
+            showTasks(statement.getSelectedList(), update.getMessage().getChatId());
+        }
+        if (message.toLowerCase().contains("/addtask")){
+            UserStatement statement = getUserStatement(update.getMessage().getFrom().getId());
+            statement.setLastCommand("/addtask");
+            sendMessage("Enter new task.", update.getMessage().getChatId());
+        }
+        if (message.toLowerCase().contains("/deletetask")){
+            UserStatement statement = getUserStatement(update.getMessage().getFrom().getId());
+            statement.setLastCommand("/deletetask");
+            sendMessage("Select task which you want to delete.", update.getMessage().getChatId());
+        }
+        if (message.toLowerCase().contains("/changetask")){
+            UserStatement statement = getUserStatement(update.getMessage().getFrom().getId());
+            statement.setLastCommand("/changetask");
+            sendMessage("Enter new task`s description.", update.getMessage().getChatId());
+        }
+        if (message.toLowerCase().contains("/changetaskstatus")){
+            UserStatement statement = getUserStatement(update.getMessage().getFrom().getId());
+            statement.setLastCommand("/changetaskstatus");
+            sendMessage("Enter new task`s status.", update.getMessage().getChatId());
         }
     }
 
@@ -223,5 +233,16 @@ public class TodolistBot extends TelegramLongPollingBot {
 
     private UserStatement getUserStatement(Integer user_id){
         return userStatementMap.get(user_id);
+    }
+
+    private void sendMessage(String responseMsg, Long chat_id){
+        SendMessage response = new SendMessage()
+                .setChatId(chat_id)
+                .setText(responseMsg);
+        try {
+            execute(response);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 }
